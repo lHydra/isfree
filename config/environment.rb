cb06bdf38672b461d612aab0de0296b6e7de5713
george <skywalqqer@yandex.ru>
2017-11-30 03:12:55 +0300

    change email setting for prod

--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,9 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'hydrath',
   :password => '89273223910q',
-  :domain => 'localhost',
+  :domain => '165.227.210.250',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
-
