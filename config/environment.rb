52713b5b5fc425e20c5c56360d285baf72100cec
george <skywalqqer@yandex.ru>
2017-11-30 19:04:14 +0300

    secret test

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USER'],
+  :user_name => ENV.fetch('SENDGRID_USER'),
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => '165.227.210.250',
   :address => 'smtp.sendgrid.net',
